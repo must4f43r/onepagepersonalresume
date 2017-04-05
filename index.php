9e931561f268bf55fbe1ef9390ae361de69195e9
Mustafa ER <mustafaerpro@gmail.com>
2017-04-05 23:23:56 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<!--
+                                                    #################
+                                               #####                 #####                                                  
+                                          #####                           #####                                                      
+             #############################                                     #############################                                           
+        #####                                                                                               #####                            
+   #####                                    Mustafa ER Tarafından Kodlanmıştır.                                  #####             
+   #####                                                                                                         #####              
+   #####                                      E-mail : mail@mustafaer.pro                                        #####                 
+        #####                                                                                               #####                 
+             #############################                                     #############################                                                                           
+                                          #####                           #####                                                        
+                                               #####                 #####                                                   
+                                                    #################     
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
+        <link href="css/bootstrap-theme.min.css" rel="stylesheet" type="text/css"/>
+        <script src="js/jquery-3.1.0.min.js" type="text/javascript"></script>
+        <script src="js/bootstrap.min.js" type="text/javascript"></script>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="css/style.css" rel="stylesheet" type="text/css"/>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.1/css/font-awesome.min.css">
+    <title>Mustafa ER</title>
+</head>
+<body>
+    <div class="navbar navbar-default navbar-fixed-top navbar-inverse" role="navigation">
+    <div class="container">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" style="color: #fff; cursor: default;" href="#">Mustafa ER - Personal Resume</a>
+        </div>
+        <div class="navbar-collapse collapse">
+            <!-- Sol Taraf -->
+            <ul class="nav navbar-right navbar-nav">
+                <li><a href="http://blog.mustafaer.pro" target=".blank">Blog</a></li>
+                <li><a href="http://portfolyo.mustafaer.pro" target=".blank">Portfolyo</a></li>
+                <li><a href="http://cv.mustafaer.pro" target=".blank">CV</a></li>
+                <li><a href="http://blog.mustafaer.pro/iletisim/" target=".blank">Connect</a></li>
+            </ul>
+            <!-- Sol Taraf Bitiş -->
+            
+        </div>
+    </div>
+</div>
+<div class="container" style="margin-top: 90px;">
+   <!--Begin About Section-->
+   <h1>About
+        <ul class="nav navbar-nav navbar-right">
+       	    <li class="col-lg-2 col-md-2 col-sm-4 col-xs-4"><a href="http://facebook.com/mustafaer53" target=".blank"><i class="fa fa-facebook-square fa-fw" aria-hidden="true"></i></a></li>
+       	    <li class="col-lg-2 col-md-2 col-sm-4 col-xs-4"><a href="http://twitter.com/mustafaer53" target=".blank"><i class="fa fa-twitter-square fa-fw" aria-hidden="true"></i></a></li>
+       	    <li class="col-lg-2 col-md-2 col-sm-4 col-xs-4"><a href="https://tr.linkedin.com/in/mustafaer/" target=".blank"><i class="fa fa-linkedin-square fa-fw" aria-hidden="true"></i></a></li>
+       	    <li class="col-lg-2 col-md-2 col-sm-4 col-xs-4"><a href="https://github.com/mustafaer/" target=".blank"><i class="fa fa-github-square fa-fw" aria-hidden="true"></i></a></li>
+       	    <li class="col-lg-2 col-md-2 col-sm-4 col-xs-4"><a href="https://plus.google.com/u/1/+MustafaER53/" target=".blank"><i class="fa fa-google-plus-square fa-fw" aria-hidden="true"></i></a></li>
+       	    <li class="col-lg-2 col-md-2 col-sm-4 col-xs-4"><a href="https://instagram.com/MustafaER53/" target=".blank"><i class="fa fa-instagram fa-fw" aria-hidden="true"></i></a></li>
+        </ul></h1><br>
+   <div style="margin-top: 35px;" class="col-lg-4 col-md-4 col-sm-12 col-xs-12">
+       <img src="img/mustafaer.jpg" class="img-thumbnail" alt="Mustafa ER - Img" width="304" height="236">
+   </div>
+   <h1 style="border: 2px solid #262626;"></h1>
+   <div class="col-lg-8 col-md-8 col-sm-12 col-xs-12">
+       <h2>My Name is Mustafa ER, and I am a Electrical - Electronical Engineering Student</h2>
+       <p>
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Rize de doğdum. İlk okulu Güneysu Ulucami İlköğretim okulunda okudum. Liseyi ise Güneysu Anadolu Öğretmen Lisesinde tamamladım. Şu anda Sakarya Üniversitesi Elektrik Elektronik Mühendisli Bölümünde eğitimime devam etmekteyim.
+	<br>
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;  Okulda aldığım eğitim haricinde HTML, CSS, PHP, ASP.NET web tasarımı ve programlama dil ve teknolojilerinde kendimi geliştirdim. Ayrıca C# ve DELPHİ ile masaüstü programlama bilgisine sahibim. Arduino, CCS C, Energia, 8051 Assembly ile donanımsal programlama bilgisine sahibim.Elektronik devre konusunda PROTEUS ile tasarım, basalabilir hale getirme konusunda bilgi sahibiyim. Ayrıca prototip oluştumak için baskı ve dizgi bilgisine sahibim.
+        <br>
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<b>"<a href="https://facebook.com/sauieee" style="text-decoration: none;">IEEE SAU Student Branch</a>"</b> ailesinde <b>"<a href="https://facebook.com/sauieeecomsoc" style="text-decoration: none;">Chairman Of Communications Society</a>"</b> görevini üstlenmekteyim.
+        <br>
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Profesyonel profilim için <a href="https://www.linkedin.com/in/mustafaer" style="text-decoration: none;">Linkedln</a> ağımı inceleyebilir, <a href="http://cv.mustafaer.pro" style="text-decoration: none;">CV</a> mi inceleyerek yeteneklerim hakkında daha fazla bilgi sahibi olabilir , <a href="http://portfolyo.mustafaer.pro" style="text-decoration: none;">PORTFOLYO</a> mu incelerek yapmış olduğum çalışmalara ulaşabilirsiniz.
+       </p>
+   </div>
+   <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+   <h1 style="border: 2px solid #262626;"></h1>
+   </div>
+</div>
+</body>
+</html>
